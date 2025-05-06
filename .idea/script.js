fbe16ea806b0de04a14e5f960125840db9dd3d83
s0282 <s028215757@gmail.com>
2025-05-07 00:22:47 +0800

    a

--- .idea/script.js
+++ .idea/script.js
@@ -2,3 +2,15 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
     e.preventDefault();
     alert('這是一個仿製頁面，請勿輸入真實帳密。');
 });
+
+function forgotPassword() {
+    alert('忘記密碼功能尚未啟用。此為仿製頁面。');
+}
+
+function goToRegister() {
+    alert('將導向註冊頁（模擬畫面）。此為仿製頁面。');
+}
+
+function showHelp() {
+    window.open('https://help.shopee.tw/', '_blank');
+}
